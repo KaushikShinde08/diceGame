15df64b77e98763b23e2a225167a7c714c9bb922
KaushikShinde08 <118788173+KaushikShinde08@users.noreply.github.com>
2022-11-25 19:41:38 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+#DESCRIPTION
+#Dice-Game: A game where as an input user will give a static number between (1-6) and then roll dice which randomly generate some value between(1-6)
+
+
+
+#CODE:
+import random
+
+print("Welcome to DICE GAME!")
+
+i = ""
+scr = 0  # Score
+count = 0
+
+while i != "n":
+    x = int(input("Enter Number from (1-6): "))
+    y = random.randint(1, 6)
+
+    if x not in range(1, 6 + 1):
+        print("Number must be between 1 to 6!")
+    else:
+        print(f"({x}, {y})")
+        if x == y:
+            scr += 1
+
+    if count >= 5:
+        i = str(input("Do you want to continue? (Y/n): "))
+    count += 1
+
+print("Game Over!")
+print("Your Score:", scr)
